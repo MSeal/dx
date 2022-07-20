c1e6c054e7c0deddd270a576a9ac9b9b47ae6225
Dave Shoup <dave.shoup@gmail.com>
2022-07-20 12:17:27 -0400

    move types up a level

--- dx/formatters/__init__.py
+++ dx/formatters/__init__.py
@@ -1,4 +1,3 @@
 from .dataresource import *
 from .dx import *
 from .main import *
-from .types import *
--- dx/types.py
+++ dx/types.py
@@ -1,10 +1,13 @@
 import enum
 
-DATARESOURCE_MEDIA_TYPE = "application/vnd.dataresource+json"
-DX_MEDIA_TYPE = "application/vnd.dex.v1+json"
-
 
 class DXDisplayMode(enum.Enum):
     enhanced = "enhanced"  # GRID display
     simple = "simple"  # classic simpleTable/DEX display
     default = "default"  # basic/vanilla python/pandas display
+
+
+class DXSamplingMode(enum.Enum):
+    first = "first"  # df.head(num_rows)
+    random = "random"  # df.sample(num_rows)
+    last = "last"  # df.tail(num_rows)
