a578b56808874b31103620284c4c5f20ad022678
Dave Shoup <dave.shoup@gmail.com>
2022-08-04 19:13:56 -0400

    add More Info user help section after truncating dataframe

--- dx/formatters/callouts.py
+++ dx/formatters/callouts.py
@@ -1,4 +1,6 @@
 import enum
+import uuid
+from typing import Optional
 
 from IPython.display import HTML, display
 from pydantic import BaseModel
@@ -10,27 +12,52 @@ class CalloutLevel(enum.Enum):
     info = "info"
     warning = "warning"
     danger = "danger"
+    success = "success"
+
+
+class CalloutIcon(enum.Enum):
+    info = "info"
+    warning = "warning"
+    success = "success"
 
 
 class Callout(BaseModel):
-    message: str
+    icon: Optional[CalloutIcon] = None
     level: CalloutLevel = CalloutLevel.info
+    message: str
+    use_header: bool = True
 
     @property
     def html(self):
-        heading_html = f"<h6 class='bp3-heading'>{self.level.value.title()}</h6>"
-        callout_classes = " ".join(
-            [
-                "bp3-callout",
-                f"bp3-intent-{self.level.value}",
-            ]
-        )
-        return f"<div class='{callout_classes}'>{heading_html}{self.message}</div>"
+        callout_classes = [
+            "bp3-callout",
+            f"bp3-intent-{self.level.value}",
+        ]
+        if self.icon is not None:
+            callout_classes.append(f"bp3-icon-{self.icon.value}-sign")
+        callout_class_str = " ".join(callout_classes)
+
+        if self.use_header:
+            heading_html = f"<h6 class='bp3-heading'>{self.level.value.title()}</h6>"
+            return (
+                f"<div class='{callout_class_str}'>{heading_html}{self.message}</div>"
+            )
+
+        return f"<div class='{callout_class_str}'>{self.message}</div>"
 
 
 def display_callout(
     message: str,
     level: CalloutLevel = CalloutLevel.info,
+    header: bool = True,
+    icon: Optional[CalloutIcon] = None,
+    display_id: str = None,
 ) -> None:
-    callout = Callout(message=message, level=level)
-    display(HTML(callout.html))
+    callout = Callout(
+        message=message,
+        level=level,
+        use_header=header,
+        icon=icon,
+    )
+    display_id = display_id or str(uuid.uuid4())
+    display(HTML(callout.html), display_id=display_id)
