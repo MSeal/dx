f54a525e4d21a2cf52b66f8f7d2b47e310389f51
Dave Shoup <dave.shoup@gmail.com>
2022-08-05 13:27:49 -0400

    rename and test default indexing

--- dx/tests/test_utils.py
+++ dx/tests/test_utils.py
@@ -1,23 +1,25 @@
 import sys
 
-from dx.formatters.utils import truncate_if_too_big
+import pandas as pd
+
+from dx.formatters.utils import is_default_index, truncate_if_too_big
 from dx.settings import get_settings
 
 settings = get_settings()
 
 
-def test_small_dataframe_is_not_truncated(sample_dataframe):
+def test_small_dataframe_is_not_truncated(sample_dataframe: pd.DataFrame):
     """
     Test that a small dataframe is not truncated.
     """
     original_size_bytes = sys.getsizeof(sample_dataframe)
     truncated_df = truncate_if_too_big(sample_dataframe)
     truncated_size_bytes = sys.getsizeof(truncated_df)
-    assert truncated_size_bytes == original_size_bytes
     assert truncated_size_bytes <= settings.MAX_RENDER_SIZE_BYTES
+    assert truncated_size_bytes == original_size_bytes
 
 
-def test_large_dataframe_is_truncated(sample_large_dataframe):
+def test_large_dataframe_is_truncated(sample_large_dataframe: pd.DataFrame):
     """
     Test that a large dataframe is truncated to below the size of
     MAX_RENDER_SIZE_BYTES.
@@ -25,11 +27,11 @@ def test_large_dataframe_is_truncated(sample_large_dataframe):
     original_size_bytes = sys.getsizeof(sample_large_dataframe)
     truncated_df = truncate_if_too_big(sample_large_dataframe)
     truncated_size_bytes = sys.getsizeof(truncated_df)
-    assert truncated_size_bytes < original_size_bytes
     assert truncated_size_bytes <= settings.MAX_RENDER_SIZE_BYTES
+    assert truncated_size_bytes < original_size_bytes
 
 
-def test_truncated_dataframe_keeps_dtypes(sample_large_dataframe):
+def test_truncated_dataframe_keeps_dtypes(sample_large_dataframe: pd.DataFrame):
     """
     Test that a truncated dataframe doesn't alter column datatypes.
     """
@@ -38,7 +40,7 @@ def test_truncated_dataframe_keeps_dtypes(sample_large_dataframe):
     assert (truncated_df.dtypes == orig_dtypes).all()
 
 
-def test_wide_dataframe_is_narrowed(sample_wide_dataframe):
+def test_wide_dataframe_is_narrowed(sample_wide_dataframe: pd.DataFrame):
     """
     Test that a wide dataframe is narrowed to below the size of
     the display mode's MAX_COLUMNS setting.
@@ -46,11 +48,11 @@ def test_wide_dataframe_is_narrowed(sample_wide_dataframe):
     orig_width = len(sample_wide_dataframe.columns)
     narrow_df = truncate_if_too_big(sample_wide_dataframe)
     narrow_width = len(narrow_df.columns)
-    assert narrow_width < orig_width
+    assert narrow_width < orig_width, f"{narrow_width=}"
     assert narrow_width <= settings.DISPLAY_MAX_COLUMNS
 
 
-def test_long_dataframe_is_shortened(sample_long_dataframe):
+def test_long_dataframe_is_shortened(sample_long_dataframe: pd.DataFrame):
     """
     Test that a long dataframe is shortened to below the size of
     the display mode's MAX_ROWS setting.
@@ -58,12 +60,12 @@ def test_long_dataframe_is_shortened(sample_long_dataframe):
     orig_length = len(sample_long_dataframe)
     short_df = truncate_if_too_big(sample_long_dataframe)
     short_length = len(short_df)
-    assert short_length < orig_length
+    assert short_length < orig_length, f"{short_length=}"
     assert short_length <= settings.DISPLAY_MAX_ROWS
 
 
 def test_long_wide_dataframe_is_reduced_from_both_dimensions(
-    sample_long_wide_dataframe,
+    sample_long_wide_dataframe: pd.DataFrame,
 ):
     """
     Test that a long wide dataframe is reduced from both dimensions
@@ -78,3 +80,20 @@ def test_long_wide_dataframe_is_reduced_from_both_dimensions(
     assert reduced_width <= settings.DISPLAY_MAX_COLUMNS
     assert reduced_length < orig_length
     assert reduced_length <= settings.DISPLAY_MAX_ROWS
+
+
+def test_default_index_returns_true(sample_dataframe: pd.DataFrame):
+    index = sample_dataframe.index
+    assert is_default_index(index)
+
+
+def test_custom_index_returns_false(sample_dataframe: pd.DataFrame):
+    sample_dataframe.set_index("col_1", inplace=True)
+    index = sample_dataframe.index
+    assert not is_default_index(index)
+
+
+def test_multiindex_returns_false(sample_dataframe: pd.DataFrame):
+    sample_dataframe.set_index(["col_1", "col_2"], inplace=True)
+    index = sample_dataframe.index
+    assert not is_default_index(index)
