945d5ac9eeb35bc3e2c17f01e9b987486121081e
Dave Shoup <dave.shoup@gmail.com>
2022-08-20 23:46:18 -0400

    refactor display formatter registration

--- dx/formatters/dataresource.py
+++ dx/formatters/dataresource.py
@@ -6,14 +6,11 @@ import numpy as np
 import pandas as pd
 import structlog
 from IPython import get_ipython
-from IPython.core.formatters import DisplayFormatter
+from IPython.core.formatters import BaseFormatter
 from IPython.core.interactiveshell import InteractiveShell
-from IPython.display import HTML
-from IPython.display import display as ipydisplay
 from pandas.io.json import build_table_schema
 from pydantic import BaseSettings, Field
 
-from dx.config import DEFAULT_IPYTHON_DISPLAY_FORMATTER, IN_IPYTHON_ENV
 from dx.filtering import SUBSET_FILTERS
 from dx.sampling import sample_and_describe
 from dx.settings import settings
@@ -56,49 +53,48 @@ dataresource_settings = get_dataresource_settings()
 logger = structlog.get_logger(__name__)
 
 
-class DXDataResourceDisplayFormatter(DisplayFormatter):
-    def format(self, obj, **kwargs):
+def handle_dataresource_format(obj):
+    if not isinstance(obj, pd.DataFrame):
+        obj = to_dataframe(obj)
 
-        if isinstance(obj, tuple(settings.RENDERABLE_OBJECTS)):
-            if not isinstance(obj, pd.DataFrame):
-                obj = to_dataframe(obj)
+    default_index_used = is_default_index(obj.index)
+    obj = normalize_index_and_columns(obj)
 
-            default_index_used = is_default_index(obj.index)
-            obj = normalize_index_and_columns(obj)
+    if not settings.ENABLE_DATALINK:
+        payload, metadata = format_dataresource(
+            obj,
+            has_default_index=default_index_used,
+        )
+        return payload, metadata
 
-            if not settings.ENABLE_DATALINK:
-                format_dataresource(
-                    obj,
-                    has_default_index=default_index_used,
-                )
-                return ({}, {})
+    obj_hash = generate_df_hash(obj)
+    update_existing_display = obj_hash in SUBSET_TO_DATAFRAME_HASH
+    applied_filters = SUBSET_FILTERS.get(obj_hash)
+    display_id = get_display_id(obj_hash)
+    sqlite_df_table = register_display_id(
+        obj,
+        display_id=display_id,
+        df_hash=obj_hash,
+        is_subset=update_existing_display,
+    )
 
-            obj_hash = generate_df_hash(obj)
-            update_existing_display = obj_hash in SUBSET_TO_DATAFRAME_HASH
-            applied_filters = SUBSET_FILTERS.get(obj_hash)
-            display_id = get_display_id(obj_hash)
-            sqlite_df_table = register_display_id(
-                obj,
-                display_id=display_id,
-                df_hash=obj_hash,
-                is_subset=update_existing_display,
-            )
+    payload, metadata = format_dataresource(
+        obj,
+        update=update_existing_display,
+        display_id=display_id,
+        filters=applied_filters,
+        has_default_index=default_index_used,
+    )
 
-            format_dataresource(
-                obj,
-                update=update_existing_display,
-                display_id=display_id,
-                filters=applied_filters,
-                has_default_index=default_index_used,
-            )
+    # this needs to happen after sending to the frontend
+    # so the user doesn't wait as long for writing larger datasets
+    store_in_sqlite(sqlite_df_table, obj)
+    return payload, metadata
 
-            # this needs to happen after sending to the frontend
-            # so the user doesn't wait as long for writing larger datasets
-            store_in_sqlite(sqlite_df_table, obj)
 
-            return ({}, {})
-
-        return DEFAULT_IPYTHON_DISPLAY_FORMATTER.format(obj, **kwargs)
+class DXDataResourceDisplayFormatter(BaseFormatter):
+    print_method = "_repr_data_resource_"
+    _return_type = (dict,)
 
 
 def generate_dataresource_body(
@@ -109,26 +105,21 @@ def generate_dataresource_body(
     Transforms the dataframe to a payload dictionary containing the
     table schema and column values as arrays.
     """
-    payload_body = {
+    payload = {
         "schema": build_table_schema(df),
         "data": df.reset_index().to_dict("records"),
-        "datalink": {},
+        "datalink": {"display_id": display_id},
     }
-    payload = {dataresource_settings.DATARESOURCE_MEDIA_TYPE: payload_body}
 
-    metadata_body = {
+    metadata = {
         "datalink": {
             "dataframe_info": {},
             "dx_settings": settings.json(exclude={"RENDERABLE_OBJECTS": True}),
             "applied_filters": [],
+            "display_id": display_id,
         },
+        "display_id": display_id,
     }
-    metadata = {dataresource_settings.DATARESOURCE_MEDIA_TYPE: metadata_body}
-
-    display_id = display_id or str(uuid.uuid4())
-    payload_body["datalink"]["display_id"] = display_id
-    metadata_body["datalink"]["display_id"] = display_id
-
     return (payload, metadata)
 
 
@@ -143,33 +134,35 @@ def format_dataresource(
     df, dataframe_info = sample_and_describe(df, display_id=display_id)
     dataframe_info["default_index_used"] = has_default_index
     payload, metadata = generate_dataresource_body(df, display_id=display_id)
-    metadata[dataresource_settings.DATARESOURCE_MEDIA_TYPE]["datalink"].update(
+    metadata["datalink"].update(
         {
             "dataframe_info": dataframe_info,
             "applied_filters": filters,
         }
     )
 
-    # don't pass a dataframe in here, otherwise you'll get recursion errors
-    with pd.option_context(
-        "html.table_schema", dataresource_settings.DATARESOURCE_HTML_TABLE_SCHEMA
-    ):
-        logger.debug(f"displaying dataresource payload in {display_id=}")
-        ipydisplay(
-            payload,
-            raw=True,
-            metadata=metadata,
-            display_id=display_id,
-            update=update,
-        )
+    # TODO: figure out a way to mimic this behavior since it was helpful
+    # having a display handle that we could update in place,
+    # but that went through as a display_data message, instead of execute_result
+    # and we can't do it with BaseFormatter, otherwise we'll double-render
+    # with pd.option_context(
+    #     "html.table_schema", dataresource_settings.DATARESOURCE_HTML_TABLE_SCHEMA
+    # ):
+    #     logger.debug(f"displaying dataresource payload in {display_id=}")
+    #     ipydisplay(
+    #         payload,
+    #         raw=True,
+    #         metadata=metadata,
+    #         display_id=display_id,
+    #         update=update,
+    #     )
 
-    # temporary placeholder for copy/paste user messaging
-    ipydisplay(
-        HTML("<div></div>"),
-        display_id=display_id + "-primary",
-        update=update,
-    )
-
+    # # temporary placeholder for copy/paste user messaging
+    # ipydisplay(
+    #     HTML("<div></div>"),
+    #     display_id=display_id + "-primary",
+    #     update=update,
+    # )
     return (payload, metadata)
 
 
@@ -179,7 +172,9 @@ def deregister(ipython_shell: Optional[InteractiveShell] = None) -> None:
     display formatter, used for simpleTable / "classic DEX" outputs
     and updates global dx & pandas settings with dataresource settings.
     """
-    if not IN_IPYTHON_ENV and ipython_shell is None:
+    from dx.formatters.dx import get_dx_settings
+
+    if get_ipython() is None and ipython_shell is None:
         return
 
     global settings
@@ -188,6 +183,7 @@ def deregister(ipython_shell: Optional[InteractiveShell] = None) -> None:
     settings_to_apply = {
         "DISPLAY_MAX_COLUMNS",
         "DISPLAY_MAX_ROWS",
+        "HTML_TABLE_SCHEMA",
         "MEDIA_TYPE",
         "RENDERABLE_OBJECTS",
         "FLATTEN_INDEX_VALUES",
@@ -200,4 +196,21 @@ def deregister(ipython_shell: Optional[InteractiveShell] = None) -> None:
         setattr(settings, setting, val)
 
     ipython = ipython_shell or get_ipython()
-    ipython.display_formatter = DXDataResourceDisplayFormatter()
+
+    # https://github.com/pandas-dev/pandas/blob/ad190575aa75962d2d0eade2de81a5fe5a2e285b/pandas/io/formats/printing.py#L244
+    # https://github.com/pandas-dev/pandas/blob/926b9ceff10d9b7a957811f0a4de3167332196de/pandas/io/formats/printing.py?q=_repr_data_resource_#L268
+    # https://ipython.readthedocs.io/en/stable/config/integrating.html#formatters-for-third-party-types
+    # https://ipython.readthedocs.io/en/stable/api/generated/IPython.display.html#:~:text=plain.for_type(int%2C%20int_formatter)
+    formatters = ipython.display_formatter.formatters
+    mimetype = dataresource_settings.DATARESOURCE_MEDIA_TYPE
+
+    formatters[mimetype] = DXDataResourceDisplayFormatter()
+    for obj in settings.RENDERABLE_OBJECTS:
+        formatters[mimetype].for_type(obj, handle_dataresource_format)
+    formatters[mimetype].enabled = True
+
+    logger.debug(f"before: {formatters=}")
+    for other_media_type in [get_dx_settings().DX_MEDIA_TYPE]:
+        if other_media_type in formatters:
+            del formatters[other_media_type]
+    logger.debug(f"after: {formatters=}")
