88066d9577689ff5e02abbd88d4a9071b4133b9f
Dave Shoup <dave.shoup@gmail.com>
2022-08-25 16:41:24 -0400

    store past samples, debug log filtering

--- dx/filtering.py
+++ dx/filtering.py
@@ -1,3 +1,4 @@
+import html
 from typing import Optional
 
 import pandas as pd
@@ -5,11 +6,13 @@ import structlog
 from IPython.display import update_display
 
 from dx.formatters.callouts import display_callout
+from dx.sampling import get_df_dimensions
 from dx.settings import get_settings, settings_context
 from dx.utils.formatting import expand_sequences
 from dx.utils.tracking import (
     DATAFRAME_HASH_TO_VAR_NAME,
     DISPLAY_ID_TO_DATAFRAME_HASH,
+    DISPLAY_ID_TO_ORIG_METADATA,
     SUBSET_TO_DATAFRAME_HASH,
     generate_df_hash,
 )
@@ -22,11 +25,40 @@ settings = get_settings()
 SUBSET_FILTERS = {}
 
 
+def store_sample_to_history(df: pd.DataFrame, display_id: str, filters: list) -> dict:
+    """
+    Updates the metadata cache to include past filters, times, and dataframe info.
+    """
+    # apply new metadata for resampled dataset
+    metadata = DISPLAY_ID_TO_ORIG_METADATA[display_id]
+    logger.debug(f"before {metadata=}")
+    datalink_metadata = metadata["datalink"]
+
+    sample_time = pd.Timestamp("now").strftime(settings.DATETIME_STRING_FORMAT)
+    sample = {
+        "sampling_time": sample_time,
+        "filters": filters,
+        "dataframe_info": get_df_dimensions(df, prefix="truncated"),
+    }
+    datalink_metadata["sample_history"].append(sample)
+    # only storing the last n-number of samples, not everything
+    datalink_metadata["sample_history"] = datalink_metadata["sample_history"][
+        -settings.NUM_PAST_SAMPLES_TRACKED :
+    ]
+    datalink_metadata["applied_filters"] = filters
+    datalink_metadata["sampling_time"] = sample_time
+
+    metadata["datalink"] = datalink_metadata
+    DISPLAY_ID_TO_ORIG_METADATA[display_id] = metadata
+    logger.debug(f"after {metadata=}")
+    return metadata
+
+
 def update_display_id(
     display_id: str,
     sql_filter: str,
     pandas_filter: Optional[str] = None,
-    filters: Optional[dict] = None,
+    filters: Optional[list] = None,
     output_variable_name: Optional[str] = None,
     limit: Optional[int] = None,
 ) -> None:
@@ -45,6 +77,12 @@ def update_display_id(
     query_string = sql_filter.format(table_name=table_name)
     logger.debug(f"sql query string: {query_string}")
     new_df = pd.read_sql(query_string, sql_engine)
+
+    with sql_engine.connect() as conn:
+        orig_df_count = conn.execute(f"SELECT COUNT (*) FROM {table_name}").scalar()
+    logger.debug(f"filtered to {len(new_df)}/{orig_df_count} row(s)")
+
+    df_metadata = store_sample_to_history(new_df, display_id=display_id, filters=filters)
 
     # in the event there were nested values stored,
     # try to expand them back to their original datatypes
@@ -67,16 +105,25 @@ def update_display_id(
     # allow temporary override of the display limit
     with settings_context(DISPLAY_MAX_ROWS=row_limit):
         logger.debug(f"updating {display_id=} with {min(row_limit, len(new_df))}-row resample")
-        update_display(new_df, display_id=display_id)
+        update_display(
+            new_df,
+            display_id=display_id,
+            metadata=df_metadata,
+        )
 
     # we can't reference a variable type to suggest to users to perform a `df.query()`
     # type operation since it was never declared in the first place
     if not df_name.startswith("unk_dataframe_"):
         # TODO: replace with custom callout media type
         output_variable_name = output_variable_name or "new_df"
-        filter_code = f"""{output_variable_name} = {df_name}.query("{pandas_filter.format(df_name=df_name)}", engine="python")"""
+        # wrapping the triple quotes internally so the user can copy/paste directly
+        # without worry of double/single quotes in their data not being handled
+        pandas_query_str = f'"""{pandas_filter.format(df_name=df_name)}"""'
+        filter_code = (
+            f"""{output_variable_name} = {df_name}.query({pandas_query_str}, engine="python")"""
+        )
         filter_msg = f"""Copy the following snippet into a cell below to save this subset to a new dataframe:
-        <pre style="background-color:white; padding:0.5rem; border-radius:5px;">{filter_code}</pre>
+        <pre style="background-color:white; padding:0.5rem; border-radius:5px;">{html.escape(filter_code, quote=True)}</pre>
         """
         display_callout(
             filter_msg,
