84a04f00044a572a4bed71d535974c6390a2954e
Dave Shoup <dave.shoup@gmail.com>
2022-09-02 16:55:36 -0400

    fix for nonunique index checking JSON serialization (#35)
    * add tests for concat dataframes
    
    * handle ValueError for duplicate index

--- dx/tests/test_formatting.py
+++ dx/tests/test_formatting.py
@@ -12,6 +12,10 @@ def test_dataresource_media_type(
     sample_dataframe: pd.DataFrame,
     get_ipython: TerminalInteractiveShell,
 ):
+    """
+    Test dataresource formatting returns the right media types
+    and doesn't fail at any point with a basic dataframe.
+    """
     payload, metadata = handle_dataresource_format(sample_dataframe, ipython_shell=get_ipython)
     assert dataresource_settings.DATARESOURCE_MEDIA_TYPE in payload
     assert dataresource_settings.DATARESOURCE_MEDIA_TYPE in metadata
@@ -21,6 +25,40 @@ def test_dx_media_type(
     sample_dataframe: pd.DataFrame,
     get_ipython: TerminalInteractiveShell,
 ):
+    """
+    Test dx formatting returns the right media types
+    and doesn't fail at any point with a basic dataframe.
+    """
     payload, metadata = handle_dx_format(sample_dataframe, ipython_shell=get_ipython)
     assert dx_settings.DX_MEDIA_TYPE in payload
     assert dx_settings.DX_MEDIA_TYPE in metadata
+
+
+def test_dataresource_nonunique_index_succeeds(
+    sample_dataframe: pd.DataFrame,
+    get_ipython: TerminalInteractiveShell,
+):
+    """
+    Test dataresource formatting doesn't fail while formatting
+    a dataframe with duplicate series and index values.
+    """
+    double_df = pd.concat([sample_dataframe, sample_dataframe])
+    try:
+        handle_dataresource_format(double_df, ipython_shell=get_ipython)
+    except Exception as e:
+        assert False, f"{e}"
+
+
+def test_dx_nonunique_index_succeeds(
+    sample_dataframe: pd.DataFrame,
+    get_ipython: TerminalInteractiveShell,
+):
+    """
+    Test dataresource formatting doesn't fail while formatting
+    a dataframe with duplicate series and index values.
+    """
+    double_df = pd.concat([sample_dataframe, sample_dataframe])
+    try:
+        handle_dx_format(double_df, ipython_shell=get_ipython)
+    except Exception as e:
+        assert False, f"{e}"
