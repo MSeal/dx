f049d5c54859e16218b65c750a75a27dc5078473
Matthew Seal <mseal007@gmail.com>
2022-09-22 21:36:00 -0700

    WIP example of dataframe renaming comms listener

--- src/dx/comms.py
+++ src/dx/comms.py
@@ -2,13 +2,15 @@ import structlog
 from IPython import get_ipython
 from IPython.core.interactiveshell import InteractiveShell
 
+import pandas as pd
+
 from dx.filtering import handle_resample
 
 logger = structlog.get_logger(__name__)
 
 
 # ref: https://jupyter-notebook.readthedocs.io/en/stable/comms.html#opening-a-comm-from-the-frontend
-def target_func(comm, open_msg):
+def resampler(comm, open_msg):
     @comm.on_msg
     def _recv(msg):
 
@@ -29,20 +31,44 @@ def target_func(comm, open_msg):
     comm.send({"connected": True})
 
 
+def renamer(comm, open_msg):
+    @comm.on_msg
+    def _recv(msg):
+
+        content = msg.get("content", {})
+        if not content:
+            return
+
+        data = content.get("data", {})
+        if not data:
+            return
+
+        data = msg["content"]["data"]
+
+        if "old_name" in data and "new_name" in data:
+            to_rename = globals().get(data["old_name"])
+            if isinstance(to_rename, pd.DataFrame):
+                globals[data["new_name"]] = to_rename
+                del globals[data["old_name"]]
+
+    comm.send({"connected": True})
+
+
 def register_comm(ipython_shell: InteractiveShell) -> None:
     """
     Registers the comm target function with the IPython kernel.
     """
     from dx.settings import get_settings
 
-    if not get_settings().ENABLE_DATALINK:
-        return
-
     if getattr(ipython_shell, "kernel", None) is None:
         # likely a TerminalInteractiveShell
         return
 
-    ipython_shell.kernel.comm_manager.register_target("datalink", target_func)
+    if get_settings().ENABLE_DATALINK:
+        ipython_shell.kernel.comm_manager.register_target("datalink", resampler)
+
+    if get_settings().ENABLE_RENAMER:
+        ipython_shell.kernel.comm_manager.register_target("rename", renamer)
 
 
 if (ipython := get_ipython()) is not None:
