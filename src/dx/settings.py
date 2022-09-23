e91a2d8a4b4356f496be85a1df88333a177ecff2
Dave Shoup <dave.shoup@gmail.com>
2022-09-23 08:59:16 -0400

    back to the original settings now that testing is done

--- src/dx/settings.py
+++ src/dx/settings.py
@@ -3,7 +3,6 @@ from contextlib import contextmanager
 from functools import lru_cache
 from typing import Optional, Set, Union
 
-import numpy as np
 import pandas as pd
 import structlog
 from IPython.core.interactiveshell import InteractiveShell
@@ -27,7 +26,7 @@ except ImportError:
 
 @lru_cache
 def get_default_renderable_types():
-    types = {np.ndarray, pd.Series, pd.DataFrame}
+    types = {pd.Series, pd.DataFrame}
     if GEOPANDAS_INSTALLED:
         gpd_types = {gpd.GeoDataFrame, gpd.GeoSeries}
         types.update(gpd_types)
@@ -35,7 +34,7 @@ def get_default_renderable_types():
 
 
 class Settings(BaseSettings):
-    LOG_LEVEL: Union[int, str] = logging.DEBUG
+    LOG_LEVEL: Union[int, str] = logging.WARNING
 
     DISPLAY_MAX_ROWS: int = 60
     DISPLAY_MAX_COLUMNS: int = 20
