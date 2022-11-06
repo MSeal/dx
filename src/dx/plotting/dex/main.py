0a3bac073783bdb5e468b24163f0ad2f7e08fdb4
Dave Shoup <dave.shoup@gmail.com>
2022-11-06 15:29:17 -0500

    export field aliases

--- src/dx/plotting/dex/main.py
+++ src/dx/plotting/dex/main.py
@@ -1,18 +1,14 @@
-from typing import Optional
-
 import pandas as pd
 import structlog
 from pydantic import parse_obj_as
 
 from dx.formatters.main import handle_format
-from dx.types.charts._base import DEXChartView
+from dx.types.charts._base import chart_view_ref
 
 logger = structlog.get_logger(__name__)
 
 
-def handle_view(
-    df: pd.DataFrame, chart: dict, return_view: bool = False, **kwargs
-) -> Optional[DEXChartView]:
+def handle_view(df: pd.DataFrame, chart: dict, return_view: bool = False, **kwargs):
     """
     Converts a chart dictionary to a DEXChartView-inherited object
     and either passes it to be handled by the display formatter,
@@ -21,14 +17,18 @@ def handle_view(
     logger.info(f"{chart=}")
 
     view = parse_obj_as(
-        DEXChartView,
+        chart_view_ref(),
         {
             "chart_mode": "line",
             "chart": chart,
             **kwargs,
         },
     )
-    view_metadata = view.dict(exclude_unset=True, exclude_none=True)
+    view_metadata = view.dict(
+        exclude_unset=True,
+        exclude_none=True,
+        by_alias=True,
+    )
     logger.info(f"{view_metadata=}")
 
     if return_view:
