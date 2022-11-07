75d69d46837f91ec7da1e5166c13249a2f740fc4
Dave Shoup <dave.shoup@gmail.com>
2022-11-07 10:16:32 -0500

    check for "index" column; start testing pie, tilemap, violin, wordcloud

--- src/dx/plotting/dex/basic.py
+++ src/dx/plotting/dex/basic.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Union
 
 import pandas as pd
 import structlog
+from pydantic.color import Color
 
 from dx.plotting.dex.main import handle_view
 from dx.types.charts._configs import (
@@ -10,10 +11,14 @@ from dx.types.charts._configs import (
     DEXFormulaDisplay,
     DEXLineSmoothing,
     DEXLineType,
+    DEXPieLabelContents,
+    DEXPieLabelType,
     DEXProBarModeType,
     DEXSortColumnsBy,
     DEXSummaryType,
+    DEXTextDataFormat,
     DEXTrendlineType,
+    DEXWordRotate,
 )
 from dx.types.charts.bar import DEXBarChartView
 from dx.types.charts.line import DEXLineChartView
@@ -34,7 +39,7 @@ def bar(
     horizontal: bool = False,
     bar_width: Optional[str] = None,
     group_other: Optional[bool] = False,
-    sort_columns_by: Optional[DEXSortColumnsBy] = "asc_col_string",
+    sort_columns_by: Optional[DEXSortColumnsBy] = "asc-col-string",
     pro_bar_mode: Optional[DEXProBarModeType] = "combined",
     combination_mode: Optional[DEXCombinationMode] = "avg",
     return_view: bool = False,
@@ -69,9 +74,9 @@ def bar(
     **kwargs
         Additional keyword arguments to pass to the metadata update.
     """
-    if str(x) not in df.columns:
+    if str(x) != "index" and str(x) not in df.columns:
         raise ValueError(f"Column '{x}' not found in DataFrame.")
-    if str(y) not in df.columns:
+    if str(y) != "index" and str(y) not in df.columns:
         raise ValueError(f"Column '{y}' not found in DataFrame.")
 
     chart_settings = {
@@ -86,7 +91,7 @@ def bar(
         chart_settings["metric3"] = bar_width
     if y2 is not None:
         chart_settings["second_bar_metric"] = y2
-        chart_settings["pro_bar_mode"] = pro_bar_mode
+        chart_settings["pro_bar_mode"] = str(pro_bar_mode).title()
 
     return handle_view(
         df,
@@ -157,13 +162,13 @@ def line(
     **kwargs
         Additional keyword arguments to pass to the metadata update.
     """
-    if str(x) not in df.columns:
+    if str(x) != "index" and str(x) not in df.columns:
         raise ValueError(f"Column '{x}' not found in DataFrame.")
 
     if isinstance(y, str):
         y = [y]
     for y_col in y:
-        if str(y_col) not in df.columns:
+        if str(y_col) != "index" and str(y_col) not in df.columns:
             raise ValueError(f"Column '{y_col}' not found in DataFrame.")
     if use_count:
         y.append("DEX_COUNT")
@@ -195,8 +200,11 @@ def line(
 
 def pie(
     df,
-    x: str,
-    y: str,
+    split_slices_by: str,
+    slice_size: str,
+    show_total: bool = True,
+    pie_label_type: Optional[DEXPieLabelType] = "rim",
+    pie_label_contents: Optional[DEXPieLabelContents] = "name",
     return_view: bool = False,
     **kwargs,
 ) -> Optional[DEXPieChartView]:
@@ -211,15 +219,26 @@ def pie(
     **kwargs
         Additional keyword arguments to pass to the plot view.
     """
-    raise NotImplementedError("Pie plots are not yet supported.")
 
-    # return handle_view(
-    #     df,
-    #     chart_mode="pie",
-    #     chart=chart_settings,
-    #     return_view=return_view,
-    #     **kwargs,
-    # )
+    if str(split_slices_by) != "index" and str(split_slices_by) not in df.columns:
+        raise ValueError(f"Column '{split_slices_by}' not found in DataFrame.")
+    if str(split_slices_by) != "index" and str(slice_size) not in df.columns:
+        raise ValueError(f"Column '{slice_size}' not found in DataFrame.")
+
+    chart_settings = {
+        "dim1": split_slices_by,
+        "metric1": slice_size,
+        "show_total": show_total,
+        "pie_label_type": pie_label_type,
+        "pie_label_contents": pie_label_contents,
+    }
+    return handle_view(
+        df,
+        chart_mode="pie",
+        chart=chart_settings,
+        return_view=return_view,
+        **kwargs,
+    )
 
 
 def scatterplot(
@@ -251,9 +270,9 @@ def scatterplot(
     **kwargs
         Additional keyword arguments to pass to the metadata update.
     """
-    if str(x) not in df.columns:
+    if str(x) != "index" and str(x) not in df.columns:
         raise ValueError(f"Column '{x}' not found in DataFrame.")
-    if str(y) not in df.columns:
+    if str(y) != "index" and str(y) not in df.columns:
         raise ValueError(f"Column '{y}' not found in DataFrame.")
 
     chart_settings = {
@@ -283,6 +302,13 @@ def tilemap(
     df,
     lat: str,
     lon: str,
+    icon_fill_color: Color = "#000000",
+    icon_opacity: float = 1.0,
+    icon_size: int = 2,
+    icon_type: str = "point",
+    stroke_color: Color = "#000000",
+    stroke_width: int = 2,
+    map_mode: str = "tile",
     return_view: bool = False,
     **kwargs,
 ) -> Optional[DEXTilemapChartView]:
@@ -293,23 +319,66 @@ def tilemap(
     ------
     df: pd.DataFrame
         The DataFrame to plot.
+    lat: str
+        The column to use for the latitude.
+    lon: str
+        The column to use for the longitude.
+    icon_fill_color: Color
+        The color to use for the icon fill.
+    icon_opacity: float
+        The opacity to use for the icon (0.0 to 1.0).
+    icon_size: int
+        The size to use for the icon. (0 to 10; default: 2)
+    icon_type: str
+        The type of icon to use. One of "point", "circle", "square", "diamond", "triangle", "star", "cross", or "x".
+    stroke_color: Color
+        The color to use for the icon stroke.
+    stroke_width: int
+        The width to use for the icon stroke.
+    map_mode: str
+        The type of map to use. One of "tile" or "satellite".
 
     **kwargs
         Additional keyword arguments to pass to the plot view.
     """
-    raise NotImplementedError("Tilemaps are not yet supported.")
+    if str(lat) != "index" and str(lat) not in df.columns:
+        raise ValueError(f"Column '{lat}' not found in DataFrame.")
+    if str(lon) != "index" and str(lon) not in df.columns:
+        raise ValueError(f"Column '{lon}' not found in DataFrame.")
 
-    # return handle_view(
-    #     df,
-    #     chart_mode="tilemap",
-    #     chart=chart_settings,
-    #     return_view=return_view,
-    #     **kwargs,
-    # )
+    layer_settings = {
+        "lat_dim": lat,
+        "long_dim": lon,
+        "color": icon_fill_color,
+        "transparency": icon_opacity,
+        "size": icon_size,
+        "type": icon_type,
+        "stroke": stroke_color,
+        "stroke_width": stroke_width,
+        # "hover_opts": {},
+        # "point_size_opts": {}
+    }
+    chart_settings = {
+        "map_mode": map_mode,
+        "layer_settings": [layer_settings],
+    }
+    return handle_view(
+        df,
+        chart_mode="tilemap",
+        chart=chart_settings,
+        return_view=return_view,
+        **kwargs,
+    )
 
 
 def violin(
     df: pd.DataFrame,
+    x: str,
+    y: str,
+    bin_count: int = 30,
+    show_interquartile_range: bool = False,
+    horizontal: bool = False,
+    sort_columns_by: Optional[DEXSortColumnsBy] = "asc-col-string",
     return_view: bool = False,
     **kwargs,
 ) -> Optional[DEXViolinChartView]:
@@ -320,23 +389,54 @@ def violin(
     ------
     df: pd.DataFrame
         The DataFrame to plot.
+    x: str
+        The column to use for the x-axis.
+    y: str
+        The column to use for the y-axis.
+    bin_count: int
+        The number of bins to use for the violin plot.
+    show_interquartile_range: bool
+        Whether to show the interquartile range.
+    horizontal: bool
+        Whether to plot the violin horizontally. (default: False)
+    sort_columns_by: Optional[DEXSortColumnsBy]
+        The method to use for sorting columns. One of "asc-col-string", "desc-col-string", "asc-col-number", "desc-col-number", "asc-col-date",
+        or "desc-col-date".
 
     **kwargs
         Additional keyword arguments to pass to the plot view.
     """
-    raise NotImplementedError("Violin plots are not yet supported.")
+    if str(x) != "index" and str(x) not in df.columns:
+        raise ValueError(f"Column '{x}' not found in DataFrame.")
+    if str(y) != "index" and str(y) not in df.columns:
+        raise ValueError(f"Column '{y}' not found in DataFrame.")
 
-    # return handle_view(
-    #     df,
-    #     chart_mode="violin",
-    #     chart=chart_settings,
-    #     return_view=return_view,
-    #     **kwargs,
-    # )
+    chart_settings = {
+        "dim1": x,
+        "metric1": y,
+        "horizontal": "horizontal" if horizontal else "vertical",
+        "summary_type": "violin",
+        "summary_bins": bin_count,
+        "violin_iqr": show_interquartile_range,
+        "sort_columns_by": sort_columns_by,
+    }
+
+    return handle_view(
+        df,
+        chart_mode="violin",
+        chart=chart_settings,
+        return_view=return_view,
+        **kwargs,
+    )
 
 
 def wordcloud(
     df: pd.DataFrame,
+    word_column: str,
+    size_column: str,
+    text_format: Optional[DEXTextDataFormat] = "sentence",
+    word_rotation: Optional[DEXWordRotate] = None,
+    random_coloring: bool = False,
     return_view: bool = False,
     **kwargs,
 ) -> Optional[DEXWordcloudChartView]:
@@ -347,16 +447,36 @@ def wordcloud(
     ------
     df: pd.DataFrame
         The DataFrame to plot.
+    word_column: str
+        The column to use for the words in the cloud.
+    size_column: str
+        The column to use for the size of the words in the cloud.
+    text_format: Optional[DEXTextDataFormat]
+        The format of the text data. Either "sentence" or "token".
+    word_rotation: Optional[DEXWordRotate]
+        The rotation to use for the words in the cloud (45, 90, "jitter", or None).
+    random_coloring: bool
+        Whether to use random coloring for the words in the cloud.
 
     **kwargs
         Additional keyword arguments to pass to the plot view.
     """
-    raise NotImplementedError("Wordclouds are not yet supported.")
+    if str(word_column) != "index" and str(word_column) not in df.columns:
+        raise ValueError(f"Column '{word_column}' not found in DataFrame.")
+    if str(size_column) != "index" and str(size_column) not in df.columns:
+        raise ValueError(f"Column '{size_column}' not found in DataFrame.")
 
-    # return handle_view(
-    #     df,
-    #     chart_mode="wordcloud",
-    #     chart=chart_settings,
-    #     return_view=return_view,
-    #     **kwargs,
-    # )
+    chart_settings = {
+        "text_data_format": text_format,
+        "token_metric": size_column,
+        "word_data": word_column,
+        "word_color": "random" if random_coloring else "none",
+        "word_rotate": word_rotation if word_rotation else "none",
+    }
+    return handle_view(
+        df,
+        chart_mode="wordcloud",
+        chart=chart_settings,
+        return_view=return_view,
+        **kwargs,
+    )
