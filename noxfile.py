6f19134f630bca98f0dad2d3a06cbbd58c549d4c
Dave Shoup <dave.shoup@gmail.com>
2022-12-07 09:51:10 -0500

    add verbose mkdocs logging

--- noxfile.py
+++ noxfile.py
@@ -59,5 +59,4 @@ def generate_coverage_xml(session: nox_poetry.Session):
 @nox_poetry.session(python="3.8")
 def publish_docs(session: nox_poetry.Session):
     session.run_always("poetry", "install", "-E", "docs", external=True)
-    session.run("ls", "-lha", ".")
-    session.run("mkdocs", "gh-deploy", "--force")
+    session.run("mkdocs", "gh-deploy", "--force", "--verbose")
