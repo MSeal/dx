777aa9bc8a23019d1a98cbc4a2f3eaec818a0b3c
Dave Shoup <dave.shoup@gmail.com>
2023-01-31 09:20:13 -0500

    remove comm registration on import (#148)

--- src/dx/__init__.py
+++ src/dx/__init__.py
@@ -13,9 +13,3 @@ __version__ = "1.3.0"
 
 configure_logging()
 set_display_mode("simple")
-
-if (ipython := get_ipython()) is not None:
-    if getattr(ipython, "kernel", None) is not None:
-        register_resampler_comm(ipython)
-        register_renamer_comm(ipython)
-        register_assignment_comm(ipython)
