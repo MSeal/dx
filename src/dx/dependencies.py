4d94832ac7c83ba2b9b4e40713c5b0190db6a3b1
Dave Shoup <dave.shoup@gmail.com>
2023-03-11 00:34:32 -0500

    fix all of this and add default types:converters

--- src/dx/dependencies.py
+++ src/dx/dependencies.py
@@ -1,21 +1,74 @@
-import pkg_resources
+from importlib.util import find_spec
+
+import pandas as pd
+import structlog
+
+logger = structlog.get_logger(__name__)
 
 
-def installed_packages():
-    return {pkg.key for pkg in pkg_resources.working_set}
+def package_installed(package_name) -> bool:
+    package_exists = find_spec(package_name) is not None
+    return package_exists
 
 
-class SoftDependencies:
-    """Determines whether or not soft-dependencies are installed.
-    Used for flagging whether or not specific formatting rules should be applied.
+def dask_installed():
+    return package_installed("dask")
+
+
+def geopandas_installed():
+    return package_installed("geopandas")
+
+
+def modin_installed():
+    return package_installed("modin")
+
+
+def polars_installed():
+    return package_installed("polars")
+
+
+def vaex_installed():
+    return package_installed("vaex")
+
+
+def get_default_renderable_types() -> dict:
+    """Return a dictionary of default renderable types,
+    including callable functions or names of methods to use
+    to convert a specific type to a pandas.DataFrame.
     """
+    types = {
+        pd.Series: None,
+        pd.DataFrame: None,
+    }
 
-    CUDF_INSTALLED = "cudf" in installed_packages()
-    DASK_INSTALLED = "dask" in installed_packages()
-    GEOPANDAS_INSTALLED = "geopandas" in installed_packages()
-    MODIN_INSTALLED = "modin" in installed_packages()
-    POLARS_INSTALLED = "polars" in installed_packages()
-    VAEX_INSTALLED = "vaex" in installed_packages()
+    if dask_installed():
+        import dask.dataframe as dd
 
+        dd_types = {dd.Series: "compute", dd.DataFrame: "compute"}
+        types.update(dd_types)
 
-soft_deps = SoftDependencies()
+    if geopandas_installed():
+        import geopandas as gpd
+
+        gpd_types = {gpd.GeoDataFrame: None, gpd.GeoSeries: None}
+        types.update(gpd_types)
+
+    if modin_installed():
+        import modin.pandas as mpd
+
+        mpd_types = {mpd.DataFrame: "_to_pandas", mpd.Series: "_to_pandas"}
+        types.update(mpd_types)
+
+    if polars_installed():
+        import polars as pl
+
+        pl_types = {pl.DataFrame: "to_pandas", pl.Series: "to_pandas"}
+        types.update(pl_types)
+
+    if vaex_installed():
+        import vaex
+
+        vaex_types = {vaex.dataframe.DataFrame: "to_pandas_df"}
+        types.update(vaex_types)
+
+    return types
