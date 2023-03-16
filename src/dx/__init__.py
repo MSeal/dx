744f5ebb1a7994c475f35a400db05c405fd18d4c
Dave Shoup <dave.shoup@gmail.com>
2023-03-16 12:05:33 -0400

    Update __init__.py

--- src/dx/__init__.py
+++ src/dx/__init__.py
@@ -1,5 +1,3 @@
-from IPython import get_ipython
-
 from .comms import *
 from .datatypes import *
 from .dx import *
@@ -12,4 +10,3 @@ from .utils import *
 __version__ = "1.3.0"
 
 configure_logging()
-set_display_mode("simple")
