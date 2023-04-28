1a01e3775b83eee063f50acc0bafb2189026bec2
Dave Shoup <dave.shoup@gmail.com>
2023-04-28 17:51:49 -0400

    fix force directed network config

--- src/dx/types/charts/force_directed_network.py
+++ src/dx/types/charts/force_directed_network.py
@@ -2,15 +2,20 @@ from typing import Literal
 
 from pydantic import Field
 
+from dx.types.charts import options
 from dx.types.charts._base import DEXChartBase
 from dx.types.dex_metadata import DEXView
 
 
 class DEXForceDirectedNetworkConfig(DEXChartBase):
     # see dx.types.charts._base.DEXChartBase for available fields and configs
+    network_type: options.DEXNetworkType = Field(
+        alias="networkType",
+        default=options.DEXNetworkType.force,
+    )
     pass
 
 
 class DEXForceDirectedNetworkChartView(DEXView):
-    chart_mode: Literal["force_directed_network"] = "force_directed_network"
+    chart_mode: Literal["network"] = "network"
     chart: DEXForceDirectedNetworkConfig = Field(default_factory=DEXForceDirectedNetworkConfig)
