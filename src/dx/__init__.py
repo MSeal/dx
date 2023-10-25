3b6e381709b46d4775779fc4f7cdf4404ef8edf4
Dave Shoup <dave.shoup@gmail.com>
2023-10-25 19:01:59 -0400

    adjust imports

--- src/dx/__init__.py
+++ src/dx/__init__.py
@@ -3,7 +3,6 @@ from dx.datatypes import *
 from dx.dx import display, show_docs
 from dx.formatters import *
 from dx.loggers import configure_logging
-from dx.plotting import *
 from dx.settings import *
 
 __version__ = "1.4.0"
--- src/dx/types/__init__.py
+++ src/dx/types/__init__.py
@@ -1,4 +1,2 @@
-from .charts import *
-from .dex_metadata import *
 from .filters import *
 from .main import *
